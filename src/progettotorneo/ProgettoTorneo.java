e206a44447bd49f8f2ea773bc6489d9ed1acd05a
binf01-Trinari <trinarichristiangmail.com>
2024-11-29 09:42:29 +0100

    commit arbitro

--- src/progettotorneo/ProgettoTorneo.java
+++ src/progettotorneo/ProgettoTorneo.java
@@ -14,6 +14,7 @@ public class ProgettoTorneo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Arbitro");
         // TODO code application logic here
     }
     
