691e00275f19d9f87aa129dbba7a65bc29a9e654
FedericoVesco <federicoveschini@gmail.com>
2024-12-10 12:41:33 +0100

    progetto

---
+++ src/veschinitorneo/Arbitro.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package veschinitorneo;
+
+/**
+ *
+ * @author B045_04
+ */
+public class Arbitro {
+    
+}
